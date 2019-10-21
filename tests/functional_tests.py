57cb91ec456b561e85177646fc36f33874be6e87
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-10-21 10:50:20 -0700

    rewriting functional_tests.py to use unittest

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class FirefoxTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_page(self):        #test method names must start with 'test'
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Django', self.browser.title)
+        # self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main()    #call unittest.main(), which launches
+    #   the unittest test runner, which will automatically find test classes and
+    #   methods in the file and run them
