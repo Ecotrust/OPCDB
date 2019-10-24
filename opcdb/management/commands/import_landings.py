b498af817b036aa954e09c34318bbcafe1907f76
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-10-24 12:25:57 -0700

    attempt to use django ORM to load data

--- opcdb/management/commands/import_landings.py
+++ opcdb/management/commands/import_landings.py
@@ -12,16 +12,9 @@ class Command(BaseCommand):
         import sys
         from opcdb.models import LandingRecord
         from datetime import datetime, date, timedelta
-        from django.db import connection
 
         def lower_first(in_string):
             return in_string[:1].lower() + in_string[1:] if in_string else ''
-
-        def cap_first(in_string):
-            return in_string[:1].upper() + in_string[1:] if in_string else ''
-
-        def escape_quotes(in_string):
-            return "\\'".join(in_string.split("''"))
 
         try:
             in_file_name = options['file']
@@ -30,13 +23,19 @@ class Command(BaseCommand):
             sys.exit()
 
         loc = ("%s" % in_file_name)
-        record_count = 0
+        created = 0
+        updated = 0
+        total_records = 0
         try:
             if 'xlsx' in in_file_name:
                 import xlrd
+                print('Opening Excel file... (%s)' % datetime.now().strftime('%H:%M:%S'))
                 wb = xlrd.open_workbook(loc, on_demand=True)
+                print('Excel file open (%s)' % datetime.now().strftime('%H:%M:%S'))
                 sheet = wb.sheet_by_index(0)
-                sheet.cell_value(0,0)
+                # sheet.cell_value(0,0)
+                nrows = sheet.nrows
+                ncols = sheet.ncols
 
                 if not sheet.cell_value(0, 0) == 'id':
                     file_has_id = True
@@ -44,115 +43,69 @@ class Command(BaseCommand):
                     file_has_id = False
 
                 header_lookup = []
-                for col_id in range(sheet.ncols):
+                print('Reading in headers...')
+                for col_id in range(ncols):
                     header_key = sheet.cell_value(0, col_id)
                     header_lookup.append({
                         'header': header_key,
                         'field': lower_first(header_key),
                     })
-
-                with connection.cursor() as cursor:
-                    for row_id in range(sheet.nrows):
-                        if row_id > 0:   #skip header row
-                            record = {}
-                            if not file_has_id:
-                                record['id'] = ''
-                            for col_id in range(sheet.ncols):
-                                key = header_lookup[col_id]
-                                record[key['field']] = sheet.cell_value(row_id, col_id)
-                                if record[key['field']] == '' or record[key['field']] == '*':
-                                    record[key['field']] = None
-                                if key['field'] == 'landingDate':
-                                    date_tuple = xlrd.xldate_as_tuple(record[key['field']], 0)
-                                    record[key['field']] = date(date_tuple[0], date_tuple[1], date_tuple[2])
-
-                            # Get required unique identifier fields
-                            required_fields = [
-                                "LandingReceiptNum",
-                                "LandingDate",
-                                "FisherID",
-                                "PortID",
-                                "BusinessID",
-                                "PrimaryGearID",
-                                "SpeciesID",
-                                "GearID",
-                                "FishConditionID",
-                                "UseID",
-                            ]
-                            if 'id' in record.keys():
-                                record.pop('id')
-                            set_fields = []
-                            # print('building SQL')
-                            for record_key in record.keys():
-                                if type(record[record_key]) == str:
-                                    record[record_key] = "'%s'" % escape_quotes(record[record_key])
-                                    set_fields.append("\"%s\" = %s" % (cap_first(record_key), record[record_key]) )
-                                elif record_key == 'landingDate':
-                                    record[record_key] = "DATE \'%s\'" % record[record_key]
-                                    set_fields.append('"%s" = %s' % (cap_first(record_key), record[record_key]) )
-                                elif record[record_key] == None:
-                                    record[record_key] = 'NULL'
-                                else:
-                                    record[record_key] = escape_quotes(str(record[record_key]))
-                                    set_fields.append('"%s" = %s' % (cap_first(record_key), record[record_key]) )
-                            required_fields_columns = '"%s"' % '", "'.join(required_fields)
-                            required_fields_values_list = []
-                            lookup_condition_values = []
-                            for column in required_fields:
-                                required_fields_values_list.append(record[lower_first(column)])
-                                if record[lower_first(column)] == 'NULL':
-                                    lookup_condition_values.append('"%s" IS NULL' % column)
-                                else:
-                                    lookup_condition_values.append('"%s" = %s' % (column, record[lower_first(column)]))
-                            required_fields_values = ', '.join(required_fields_values_list)
-                            lookup_condition = ' AND '.join(lookup_condition_values)
-                            get_or_create_SQL = 'INSERT INTO opcdb_landingrecord (%s) SELECT %s WHERE NOT EXISTS ( SELECT id from opcdb_landingrecord WHERE %s);' % (required_fields_columns, required_fields_values, lookup_condition)
-                            cursor.execute(get_or_create_SQL)
-                            # (instance, new) = LandingRecord.objects.get_or_create(
-                            #     landingReceiptNum = record['landingReceiptNum'],
-                            #     landingDate = record['landingDate'],
-                            #     fisherId = record['fisherID'],
-                            #     portID = record['portID'],
-                            #     businessID = record['businessID'],
-                            #     primaryGearID = record['primaryGearID'],
-                            #     speciesID = record['speciesID'],
-                            #     gearID = record['gearID'],
-                            #     fishConditionID = record['fishConditionID'],
-                            #     useID = record['useID'],
-                            # )
-
-                            # instance_id = instance.pk
-                            SQL_command = 'UPDATE opcdb_landingrecord SET'
+                print('Headers loaded')
 
 
-                                # setattr(instance, record_key, record[record_key])
-                            # print('assembling SQL')
-                            SQL_command = "%s %s WHERE %s; COMMIT;" % (SQL_command, ', '.join(set_fields), lookup_condition)
-                            # print('running SQL')
-                            cursor.execute(SQL_command)
-                            # print('SQL run')
-                            # instance.save()
+                for row_id in range(nrows):
+                    if row_id > 0:   #skip header row
+                        record = {}
+                        if not file_has_id:
+                            record['id'] = ''
+                        for col_id in range(ncols):
+                            key = header_lookup[col_id]
+                            record[key['field']] = sheet.cell_value(row_id, col_id)
+                            if record[key['field']] == '' or record[key['field']] == '*':
+                                record[key['field']] = None
+                            if key['field'] == 'landingDate':
+                                date_tuple = xlrd.xldate_as_tuple(record[key['field']], 0)
+                                record[key['field']] = date(date_tuple[0], date_tuple[1], date_tuple[2])
 
-                            if record_count%2000 == 0:
-                                percent = int((record_count/sheet.nrows)*100)
-                                print('Running: %d%% (%s)' % (percent, datetime.now().strftime('%H:%M:%S')))
+                        # TODO: Get required unique identifier fields
+                        (instance, new) = LandingRecord.objects.get_or_create(
+                            landingReceiptNum = record['landingReceiptNum'],
+                            landingDate = record['landingDate'],
+                            fisherId = record['fisherID'],
+                            portID = record['portID'],
+                            businessID = record['businessID'],
+                            primaryGearID = record['primaryGearID'],
+                            speciesID = record['speciesID'],
+                            gearID = record['gearID'],
+                            fishConditionID = record['fishConditionID'],
+                            useID = record['useID'],
+                        )
+                        if 'id' in record.keys():
+                            record.pop('id')
+                        for record_key in record.keys():
+                            setattr(instance, record_key, record[record_key])
+                        instance.save()
 
-                            record_count += 1
+                        if total_records%2000 == 0:
+                            percent = int((total_records/nrows)*100)
+                            print('Running: %d%% (%s)' % (percent, datetime.now().strftime('%H:%M:%S')))
 
+                        total_records += 1
+                        if new:
+                            created += 1
+                        else:
+                            updated += 1
 
-                    # return True
-                print("record_count: %s" % record_count)
+                # return True
+                print("Created: %s; Updated %s records" % (created, updated))
 
         except Exception as e:
             print(e)
-            if 'get_or_create_SQL' in locals():
-                print("Get/Create SQL: %s" % get_or_create_SQL)
-            if 'SQL_command' in locals():
-                print("SQL Command: %s" % SQL_command)
-            print("record_count: %s" % record_count)
             pass
+
         finally:
-            wb.release_resources()
+            if 'wb' in locals():
+                wb.release_resources()
 
 
         try:
